f0eb70c8566f2e33f0164e6b88d336da0371778d
jamiebell941 <jamiebell941@gmail.com>
2020-05-07 09:14:20 +0100

    tried extensions got confused

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,17 @@
+import behaviours.IReviewed;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    public ArrayList<IReviewed> parkFeatures;
+
+    public ThemePark(ArrayList<IReviewed> parkFeatures) {
+        this.parkFeatures = parkFeatures;
+    }
+
+//    public ArrayList<IReviewed> getAllReviewed(){
+//        for (IReviewed reviewed: parkFeatures)
+//            return this.parkFeatures.getRating();
+//    }
 }
